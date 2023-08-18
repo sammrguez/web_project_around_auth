62cd582a86089ffe3202958d113678a6a21ea551
sammrguez <sammrguez@gmail.com>
2023-08-18 13:49:50 -0600

    PopuupWithForm  has just been created

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,73 +2,99 @@ import addIcon from "../images/add_button.svg";
 import editIcon from "../images/edit_button.png";
 
 function Main() {
+  function handleEditAvatarClick() {
+    document
+      .querySelector(".popup_type_update-avatar")
+      .classList.add("popup_opened");
+  }
+  function handleEditProfileClick() {
+    document
+      .querySelector(".popup_type_edit-profile")
+      .classList.add("popup_opened");
+  }
+  function handleAddPlaceClick() {
+    document
+      .querySelector(".popup_type_new-place")
+      .classList.add("popup_opened");
+  }
+
+  //visual//
   return (
-    <main class="content">
-      <section class="profile">
-        <div class="profile__avatar-overlay"></div>
+    <main className="content">
+      <section className="profile">
+        <div
+          className="profile__avatar-overlay"
+          onClick={handleEditAvatarClick}
+        ></div>
         <img
-          class="profile__avatar"
+          className="profile__avatar"
           src=" "
           alt="aquí está tu foto de perfil"
         />
 
-        <div class="profile__info">
-          <div class="edit-button">
+        <div className="profile__info">
+          <div className="edit-button">
             <img
-              class="edit-button__icon"
+              className="edit-button__icon"
               src={editIcon}
               alt="ícono de editar"
+              onClick={handleEditProfileClick}
             />
           </div>
-          <h1 class="profile__user-name"></h1>
-          <h2 class="profile__user-profession"></h2>
+          <h1 className="profile__user-name"></h1>
+          <h2 className="profile__user-profession"></h2>
         </div>
-        <div class="add-button">
-          <img class="add-button__icon" src={addIcon} alt="ícono de agregar" />
+        <div className="add-button">
+          <img
+            className="add-button__icon"
+            src={addIcon}
+            alt="ícono de agregar"
+            onClick={handleAddPlaceClick}
+          />
         </div>
       </section>
 
-      <section class="card-container"></section>
+      <section className="card-container"></section>
 
-      <section class="popups">
-        <div class="popup popup_type_edit-profile">
-          <div class="overlay"></div>
+      <section className="popups">
+        <div className="popup popup_type_edit-profile">
+          <div className="overlay"></div>
 
-          <form class="form" id="profile" novalidate name="profile">
-            <button class="form__close-button"></button>
+          <form className="form" id="profile" noValidate name="profile">
+            <button className="form__close-button"></button>
 
-            <div class="form__container">
-              <h3 class="form__header">Editar Perfil</h3>
+            <div className="form__container">
+              <h3 className="form__header">Editar Perfil</h3>
 
-              <fieldset class="form__input-container">
+              <fieldset className="form__input-container">
                 <input
                   type="text"
-                  class="form__input"
+                  className="form__input"
                   placeholder="Nombre"
                   id="name-input"
                   name="name"
                   required
-                  minlength="2"
-                  maxlength="30"
+                  minLength="2"
+                  maxLength="30"
                 />
 
-                <span class="form__input-error name-input-error"></span>
+                <span className="form__input-error name-input-error"></span>
 
                 <input
                   type="text"
-                  class="form__input"
+                  className="form__input"
                   placeholder="Acerca de mi"
                   id="about-me-input"
                   name="about-me"
                   required
-                  minlength="2"
-                  maxlength="20"
+                  minLength="2"
+                  maxLength="20"
                 />
 
-                <span class="form__input-error about-me-input-error"></span>
+                <span className="form__input-error about-me-input-error"></span>
 
                 <button
-                  class="form__submit-button form__submit-button_edit"
+                  className="form__submit-button form__submit-button_edit"
                   type="submit"
                 >
                   Crear
@@ -78,58 +104,58 @@ function Main() {
           </form>
         </div>
 
-        <div class="popup popup_type_new-place">
-          <div class="overlay"></div>
+        <div className="popup popup_type_new-place">
+          <div className="overlay"></div>
           <template id="card-template">
-            <div class="place-card">
-              <img class="place-card__photo" src=" " alt=" " />
+            <div className="place-card">
+              <img className="place-card__photo" src=" " alt=" " />
 
-              <button class="trash-button"></button>
+              <button className="trash-button"></button>
 
-              <div class="place-card__info-container">
-                <h3 class="place-card__name"></h3>
+              <div className="place-card__info-container">
+                <h3 className="place-card__name"></h3>
 
-                <button class="like-button">
-                  <span class="like-button-counter"></span>
+                <button className="like-button">
+                  <span className="like-button-counter"></span>
                 </button>
               </div>
             </div>
           </template>
-          <form class="form" id="place" novalidate name="place">
-            <button class="form__close-button form__close-button_type_new-place"></button>
+          <form className="form" id="place" noValidate name="place">
+            <button className="form__close-button form__close-button_type_new-place"></button>
 
-            <div class="form__container">
-              <h3 class="form__header">Nuevo Lugar</h3>
+            <div className="form__container">
+              <h3 className="form__header">Nuevo Lugar</h3>
 
-              <fieldset class="form__input-container">
+              <fieldset className="form__input-container">
                 <input
                   type="text"
-                  class="form__input"
+                  className="form__input"
                   placeholder="Título"
                   id="place-name-input"
                   name="placeName"
-                  minlength="4"
-                  maxlength="30"
+                  minLength="4"
+                  maxLength="30"
                   required
                 />
 
-                <span class="form__input-error place-name-input-error"></span>
+                <span className="form__input-error place-name-input-error"></span>
 
                 <input
                   type="url"
-                  class="form__input"
+                  className="form__input"
                   placeholder="Enlace a la Imagen"
                   id="photo-link-input"
                   name="link"
                   required
-                  minlength="4"
+                  minLength="4"
                 />
 
-                <span class="form__input-error photo-link-input-error"></span>
+                <span className="form__input-error photo-link-input-error"></span>
 
                 <button
                   type="submit"
-                  class="form__submit-button form__submit-button_place"
+                  className="form__submit-button form__submit-button_place"
                   id="new-place"
                 >
                   Crear
@@ -138,21 +164,25 @@ function Main() {
             </div>
           </form>
         </div>
-        <div class="popup popup_type_delete">
+        <div className="popup popup_type_delete">
           <div class="overlay"></div>
 
-          <div class="form form_action_delete" id="deleteForm" name="delete">
+          <div
+            className="form form_action_delete"
+            id="deleteForm"
+            name="delete"
+          >
             <button
-              class="form__close-button form__close-button_type_delete"
+              className="form__close-button form__close-button_type_delete"
               type="submit"
             ></button>
 
-            <div class="form__container">
-              <h3 class="form__header">¿Estás seguro?</h3>
+            <div className="form__container">
+              <h3 className="form__header">¿Estás seguro?</h3>
 
               <button
                 type="submit"
-                class="form__submit-button form__submit-button_delete"
+                className="form__submit-button form__submit-button_delete"
                 id="delete"
               >
                 Sí
@@ -160,36 +190,36 @@ function Main() {
             </div>
           </div>
         </div>
-        <div class="popup popup_type_update-avatar popup-opened">
-          <div class="overlay"></div>
+        <div className="popup popup_type_update-avatar">
+          <div className="overlay"></div>
 
           <form
-            class="form form_action_avatar"
+            className="form form_action_avatar"
             id="avatar"
-            novalidate
+            noValidate
             name="avatar"
           >
-            <button class="form__close-button form__close-button_type_new-place"></button>
+            <button className="form__close-button form__close-button_type_new-place"></button>
 
-            <div class="form__container">
-              <h3 class="form__header">Editar foto de perfil</h3>
+            <div className="form__container">
+              <h3 className="form__header">Editar foto de perfil</h3>
 
-              <fieldset class="form__input-container">
+              <fieldset className="form__input-container">
                 <input
                   type="url"
-                  class="form__input"
+                  className="form__input"
                   placeholder="Link de foto"
                   id="avatar-input"
                   name="updateAvatar"
                   required
-                  minlength="4"
+                  minLength="4"
                 />
 
-                <span class="form__input-error avatar-input-error"></span>
+                <span className="form__input-error avatar-input-error"></span>
 
                 <button
                   type="submit"
-                  class="form__submit-button form__submit-button_avatar"
+                  className="form__submit-button form__submit-button_avatar"
                   id="updateAvatar"
                 >
                   Guardar
@@ -198,13 +228,13 @@ function Main() {
             </div>
           </form>
         </div>
-        <div class="popup popup_type_photo">
-          <div class="popup__photo-container">
-            <button class="popup__close-button"></button>
+        <div className="popup popup_type_photo">
+          <div className="popup__photo-container">
+            <button className="popup__close-button"></button>
 
-            <img class="popup__photo" src=" " />
+            <img className="popup__photo" src=" " />
 
-            <h4 class="popup__photo-caption"></h4>
+            <h4 className="popup__photo-caption"></h4>
           </div>
         </div>
       </section>
