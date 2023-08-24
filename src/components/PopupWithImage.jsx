fed27897c78bfde34df7019b094598b8a8b21273
sammrguez <sammrguez@gmail.com>
2023-08-24 17:59:57 -0600

    close popups

--- src/components/PopupWithImage.jsx
+++ src/components/PopupWithImage.jsx
@@ -1,25 +1,30 @@
 function PopupWithImage({ name, selectedCard, onClose }) {
   return (
-    <div
-      className={`popup popup_type_${name} ${
-        selectedCard ? "popup_opened" : ""
-      }`}
-    >
-      <div className="popup__photo-container">
-        {selectedCard && (
-          <>
-            <button className="popup__close-button" onClick={onClose}></button>
-            <img
-              className="popup__photo"
-              src={selectedCard.link}
-              alt={selectedCard.name}
-            />
+    <>
+      <div
+        className={`popup popup_type_${name} ${
+          selectedCard ? "popup_opened" : ""
+        }`}
+      >
+        <div className="popup__photo-container">
+          {selectedCard && (
+            <>
+              <button
+                className="popup__close-button"
+                onClick={onClose}
+              ></button>
+              <img
+                className="popup__photo"
+                src={selectedCard.link}
+                alt={selectedCard.name}
+              />
 
-            <h4 className="popup__photo-caption">{selectedCard.name} </h4>
-          </>
-        )}
+              <h4 className="popup__photo-caption">{selectedCard.name} </h4>
+            </>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
